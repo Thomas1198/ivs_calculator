127cdbcb4cc7c0565557032cdfd12454e3845c95
schovancova <sima.chovancova@gmail.com>
2019-04-22 12:21:12 +0200

    Math module functions added

--- src/main/modules/math.py
+++ src/main/modules/math.py
@@ -1,4 +1,58 @@
-"""Math module"""
+"""@file math.py
+@brief Module for math calculations.
+@author xchova20
+@date 22.4. 2019
+"""
+
+from decimal import Decimal as D
 
 def calculate(expr):
 	return str(eval(expr))
+
+def add(x, y):
+	"""Adds two numbers.
+	@param x first number.
+	@param y second number.
+	@return Decimal result
+	"""
+	return D(str(x)) + D(str(y))
+
+def multiply(x, y):
+	"""Multiplies two numbers.
+	@param x first number.
+	@param y second number.
+	@return Decimal result
+	"""
+	return D(str(x)) * D(str(y))
+
+def divide(x, y):
+	"""Divides two numbers.
+	@param x first number.
+	@param y second number.
+	@return Decimal result
+	@exceptions ZeroDivisionError in case of second number being 0.
+	"""
+	if y == 0:
+		raise ZeroDivisionError
+	return D(str(x)) / D(str(y))
+
+def subtract(x, y):
+	"""SUbtracts two numbers.
+	@param x first number.
+	@param y second number.
+	@return Decimal result
+	"""
+	return D(str(x)) - D(str(y))
+
+def factorial(x):
+	"""Creates a factorial.
+	@param x first number, greater or equal to zero and integer.
+	@return Decimal result
+	@exceptions ValueError when x is not an integer or greater/equal to 0
+	"""
+	if not isinstance(x, int) or x < 0:
+		raise ValueError("Factorial requires an integer greater or equal to 0")
+	result = 1
+	if x == 0:
+		return 1
+	return factorial(x * factorial(x-1))
