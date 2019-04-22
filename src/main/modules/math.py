3959a2c7ea1f13456bcae786566207d24b43168c
schovancova <sima.chovancova@gmail.com>
2019-04-22 13:26:49 +0200

    All math functions return float

--- src/main/modules/math.py
+++ src/main/modules/math.py
@@ -13,69 +13,77 @@ def add(x, y):
 	"""Adds two numbers.
 	@param x first number.
 	@param y second number.
-	@return Decimal result
+	@return float result
 	"""
-	return D(str(x)) + D(str(y))
+	return float(D(str(x)) + D(str(y)))
 
 def multiply(x, y):
 	"""Multiplies two numbers.
 	@param x first number.
 	@param y second number.
-	@return Decimal result
+	@return float result
 	"""
-	return D(str(x)) * D(str(y))
+	return float(D(str(x)) * D(str(y)))
 
 def divide(x, y):
 	"""Divides two numbers.
 	@param x first number.
 	@param y second number.
-	@return Decimal result
+	@return float result
 	@exceptions ZeroDivisionError in case of second number being 0.
 	"""
 	if y == 0:
 		raise ZeroDivisionError
-	return D(str(x)) / D(str(y))
+	return float(D(str(x)) / D(str(y)))
 
 def subtract(x, y):
 	"""SUbtracts two numbers.
 	@param x first number.
 	@param y second number.
-	@return Decimal result
+	@return float result
 	"""
-	return D(str(x)) - D(str(y))
+	return float(D(str(x)) - D(str(y)))
 
 def factorial(x):
 	"""Creates a factorial.
 	@param x first number, greater or equal to zero and integer.
-	@return Decimal result
+	@return float result
 	@exceptions ValueError when x is not an integer or greater/equal to 0
 	"""
 	if not isinstance(x, int) or x < 0:
 		raise ValueError("Factorial requires an integer greater or equal to 0")
-	result = 1
 	if x == 0:
 		return 1
-	return factorial(x * factorial(x-1))
+	return x * factorial(x-1)
 
 def power(x, y):
 	"""Raises number to a given power.
 	@param x number.
 	@param y power value.
-	@return Decimal result
+	@return float result
 	"""
-	return D(str(x)) ** D(str(y))
+	return float(D(str(x)) ** D(str(y)))
 
 def square(x):
 	"""Gives square root of a number.
-	@@aram x number.
-	@return Decimal result
+	@param x number greater or equal to 0.
+	@return float result
+	@exceptions ValueError when x is not greater or equal to 0
 	"""
-	return D(str(x)) ** D(1/2)
+	if x < 0:
+		raise ValueError
+	return float(D(str(x)) ** D(1/2))
 
 def nth_root(x, y):
 	"""Gives nth root of a number.
-	@param x number.
-	@param y n number
-	@return Decimal result
+	@param x number greater or equal to 0.
+	@param y n number, cannot be 0
+	@return float result
+	@exceptions ValueError when x is not greater or equal to 0
+	@exceptions ZeroDivisionError when y is 0
 	"""
-	return D(str(x)) ** D(1/Decimal(str(y)))
+	if x < 0:
+		raise ValueError
+	if y == 0:
+		raise ZeroDivisionError
+	return float(D(str(x)) ** D(1/D(str(y))))
