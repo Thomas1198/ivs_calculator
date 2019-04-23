6c35d02edc306801fc3333d84a0c7dfb44e90b70
awariss <jena.osolso@gmail.com>
2019-04-23 21:10:27 +0200

    Profilování + zprava z profilovani

---
+++ profiling/vypocet/smerodatna_odchylka.py
@@ -0,0 +1,27 @@
+from main.modules.math import *
+
+# vypocte smerodatnou odchylku z cisel vstupniho souboru
+
+import sys
+
+f=open('1000cisel.txt','r')
+num = f.readline()
+list=[]
+
+list=num.split()
+soucet=0
+
+for num in list:
+    soucet=add(soucet,int(num))
+
+pocet=len(list)
+print(pocet)
+prumer=divide(soucet,pocet)
+
+sum=0
+for num in list:
+    pom=subtract(int(num), prumer)
+    sum=add(sum,power(pom, 2))
+
+s=square(multiply(divide(1,subtract(pocet, 1)),sum))
+print(s)
